079da135a4041a000461b5241d02da4c3fe60f73
SLS\niklil <niklas.liljestrand@sls.fi>
2021-04-27 16:26:35 +0300

    add tool for viewing projects

--- src/app/components/grids/project-grid/project-grid.component.ts
+++ src/app/components/grids/project-grid/project-grid.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProjectService } from '../../../services/project.service';
 @Component({
   selector: 'app-project-grid',
   templateUrl: './project-grid.component.html',
@@ -7,8 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectGridComponent implements OnInit {
 
-  constructor() { }
+  private projects: [];
 
-  ngOnInit() {}
+  projectColumns = [
+      { headerName: 'ID', field: 'id', sortable: true, filter: true  },
+      { headerName: 'Name', field: 'name', sortable: true, filter: true  },
+      { headerName: 'Date Created', field: 'date_created', sortable: true, filter: true  },
+      { headerName: 'Date Modified', field: 'date_modified', sortable: true, filter: true  },
+      { headerName: 'Published', field: 'published', sortable: true, filter: true  },
+      { headerName: 'Deleted', field: 'deleted', sortable: true, filter: true  }
+  ];
+
+  constructor(private projectService: ProjectService) {
+    this.getProjects();
+  }
+
+  ngOnInit() {
+  }
+
+  async getProjects() {
+    this.projectService.getProjects().subscribe(
+      async (res) => {
+        this.projects = res;
+      },
+      async (res) => {
+
+      }
+    );
+  }
 
 }
